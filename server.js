5d6032cb1c56e85edae7e8b38aedab76d32b11df
Ganesha moorthy <ganesh.moorthy2552@gmail.com>
2021-04-29 14:18:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use('/api/files',folders);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.use('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname + '/client/build/index.html'))
+    res.sendFile(paths.resolve(__dirname + '/client/build/index.html'))
   });
 }
 
